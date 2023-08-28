1732d8fb8fc75c899c31b0d1ac0cef37d5ee11c3
Ashish Khatri <60392989+iamashishjava@users.noreply.github.com>
2023-08-28 23:36:02 +0545

    Add files via upload

---
+++ Product/products.js
@@ -0,0 +1,47 @@
+const products = [
+  {
+    id: 0,
+    name: "Yomari",
+    price: 100,
+    instock: 100,
+    description:
+      "A very delicious newari dessert.",
+    imgSrc: "./img/yomari.jfif",
+  },
+  {
+    id: 1,
+    name: "Bara",
+    price: 60,
+    instock: 100,
+    description:
+      "A very delicious snacks made from lentils.",
+    imgSrc: "./img/Bara.jfif",
+  },
+  {
+    id: 2,
+    name: "Khaja Set",
+    price: 300,
+    instock: 20,
+    description:
+      "Hungry? Grab our customized plate.",
+    imgSrc: "./img/Khaja.jfif",
+  },
+  {
+    id: 3,
+    name: "Sel Roti",
+    price: 200,
+    instock: 100,
+    description:
+      "National sweet delicacy. Sel Roti!!!",
+    imgSrc: "./img/Sel.jfif",
+  },
+  {
+    id: 4,
+    name: "Mo:Mo",
+    price: 200,
+    instock: 20,
+    description:
+      "Are you a Nepali? Your favorite food is here!!!!",
+    imgSrc: "./img/Momo.jpg",
+  },
+];
